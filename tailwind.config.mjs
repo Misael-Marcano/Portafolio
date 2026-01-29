c6ea614473f3ee9275f1c55a675c207675b021d3
Misael Monción Marcano <moncionm@banfondesa.com.do>
2026-01-29 18:49:00 -0400

    Primer Commit

---
+++ tailwind.config.mjs
@@ -0,0 +1,10 @@
+// tailwind.config.mjs
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  darkMode: "class", // Enable dark mode with the 'dark' class
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
