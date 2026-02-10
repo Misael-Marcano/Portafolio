7df6262049821574dd8adf95a5cfd2c9ed4afcac
Misael Monción Marcano <moncionm@banfondesa.com.do>
2026-02-10 14:32:05 -0400

    Correcion git page no se ve

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   vite: { plugins: [tailwindcss()] },
-  site: "https://misael-marcano.github.io",
+  site: "https://github.com/Misael-Marcano",
   base: "/Portafolio",
 });
